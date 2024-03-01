37d847cd2fb5647ac68608c68e02fd1238434d28
RichiCod3s <aaronrichardsonsg@gmail.com>
2024-03-01 22:28:25 +0800

    Part 8/9 completed
    Print keys and object values of a HashMap

--- part08-Part08_08.PrintMeMyHashmap/src/main/java/Program.java
+++ part08-Part08_08.PrintMeMyHashmap/src/main/java/Program.java
@@ -5,6 +5,42 @@ public class Program {
 
     public static void main(String[] args) {
         // Test your program here!
+        HashMap<String, String> hashmap = new HashMap<>();
+        hashmap.put("f.e", "for example");
+        hashmap.put("etc.", "and so on");
+        hashmap.put("i.e", "more precisely");
+
+        printKeys(hashmap);
+        System.out.println("---");
+        printKeysWhere(hashmap, "i");
+        System.out.println("---");
+        printValuesOfKeysWhere(hashmap, ".e");
     }
 
+    //prints all the keys in the hashmap
+    public static void printKeys(HashMap<String, String> hashmap) {
+        for (String key : hashmap.keySet()) {
+            System.out.println(key);
+        }
+    }
+
+    //prints the keys in the hashmap which contain the string given as a parameter.
+    public static void printKeysWhere(HashMap<String, String> hashmap, String text) {
+        
+        for (String key : hashmap.keySet()) {
+            if (key.contains(text)) {
+                System.out.println(key);
+            }
+        }
+    }
+
+    //prints the values in the hashmap whichs keys contain the given string.
+    public static void printValuesOfKeysWhere(HashMap<String, String> hashmap, String text) {
+
+        for (String key : hashmap.keySet()) {
+            if (key.contains(text)) {
+                System.out.println(hashmap.get(key));
+            }
+        }
+    }
 }
--- part08-Part08_09.PrintMeAnotherHashmap/src/main/java/Program.java
+++ part08-Part08_09.PrintMeAnotherHashmap/src/main/java/Program.java
@@ -5,6 +5,42 @@ public class Program {
 
     public static void main(String[] args) {
         // Test your program here!
+        HashMap<String, Book> hashmap = new HashMap<>();
+        hashmap.put("sense", new Book("Sense and Sensibility", 1811, "..."));
+        hashmap.put("prejudice", new Book("Pride and prejudice", 1813, "...."));
+
+        printValues(hashmap);
+        System.out.println("---");
+        printValueIfNameContains(hashmap, "prejud");
     }
 
-}
+    // prints all the values in the hashmap given as a parameter using
+    public static void printValues(HashMap<String, Book> hashmap) {
+
+        for (String key : hashmap.keySet()) {
+            System.out.println(hashmap.get(key));
+        }
+        
+        /* other way...
+        for (Book value : hashmap.values()) {
+            System.out.println(value);*/
+    }
+
+    public static void printValueIfNameContains(HashMap<String, Book> hashmap, String text) {
+       // get the set of keys
+        for (String key : hashmap.keySet()) {
+            // if the value of the books name contains the text, print the book object
+            if (hashmap.get(key).getName().contains(text)) {
+                System.out.println(hashmap.get(key));
+            }
+        }
+        
+         /* other way... 
+        for (Book value : hashmap.values()) {
+            if (value.getName().contains(text)) {
+                System.out.println(value);
+            }
+        }*/
+    }
+
+}//class
