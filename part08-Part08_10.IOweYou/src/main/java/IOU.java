35e8f5b30dac09dc74bda91678123060c053d8f9
RichiCod3s <aaronrichardsonsg@gmail.com>
2024-03-02 14:49:25 +0800

    Part8/10 complete (Section 1)
    Learned getOrDefault() when using a hashmap.
    
    owed.getOrDefault(toWhom,0.0);
    
    The second value is the default if the key is not found. This stops a Null Pointer exception

---
+++ part08-Part08_10.IOweYou/src/main/java/IOU.java
@@ -0,0 +1,25 @@
+
+import java.util.HashMap;
+
+public class IOU {
+/*The IOU does not care about old debt. When you set a new sum owed to a person 
+  when there is some money already owed to the same person, the old debt is forgotten.*/
+    private HashMap<String, Double> owed;
+
+    public IOU() {
+        this.owed = new HashMap<>();
+    }
+
+//saves the amount owed and the person owed to to the IOU.
+    public void setSum(String toWhom, double amount) {
+        this.owed.put(toWhom, amount);
+    }
+
+//returns the amount owed to the person whose name is given as a parameter. If the person
+    public double howMuchDoIOweTo(String toWhom) {
+        // getOrDefault returns 0.0 if toWhom is not know
+        // eliminated nullPointerException
+      return  this.owed.getOrDefault(toWhom,0.0);
+    }
+
+}
