d069835deb1d10929d4cb45d19b2747a29bcc711
RichiCod3s <aaronrichardsonsg@gmail.com>
2024-03-07 17:41:08 +0800

    Part 8 completed
    Part 8/15 uses ArrayLists with HashMaps

--- part08-Part08_15.StorageFacility/src/main/java/StorageFacility.java
+++ part08-Part08_15.StorageFacility/src/main/java/StorageFacility.java
@@ -3,19 +3,46 @@ import java.util.HashMap;
 
 public class StorageFacility {
 
-    HashMap<String, ArrayList<String>> storage;
+   private HashMap<String, ArrayList<String>> storage;
     
     public StorageFacility(){
-        storage = new HashMap<>();
+        this.storage = new HashMap<>();
     }
     
+    //adds item to the storage unit
     public void add(String unit, String item){
         // if the unit is not in HashMap create one
-        storage.putIfAbsent(unit, new ArrayList<>());
+        this.storage.putIfAbsent(unit, new ArrayList<>());
         
         //add item to unit's ArrayList
-       storage.get(unit).add(item);       
+       this.storage.get(unit).add(item);       
     }
-
     
+    // returns a list that contains all the items in the storage unit 
+    public ArrayList<String> contents(String storageUnit){
+        
+        return this.storage.getOrDefault(storageUnit, new ArrayList<>());
+    }
+    
+    //removes the given item from the given storage unit. 
+    public void remove(String storageUnit, String Item){
+        
+        // remove item from StorageUnit ArrayList
+        this.storage.get(storageUnit).remove(Item);
+        
+        // if array is empty, remove the storageUnit from hashMap
+        if(this.storage.get(storageUnit).isEmpty()){
+            this.storage.remove(storageUnit);
+        }    
+    }
+        //returns the names of the storage units as a list.
+         public ArrayList<String> storageUnits(){
+         ArrayList<String> storageUnitNames = new ArrayList<>();
+         
+             for(String storageUnits: this.storage.keySet()){
+            storageUnitNames.add(storageUnits);
+         }
+             
+             return storageUnitNames;
+     }  
 }
