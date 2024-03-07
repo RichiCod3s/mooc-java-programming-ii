1df2e7914d27bc1572669492d49eaac5aac94c40
RichiCod3s <aaronrichardsonsg@gmail.com>
2024-03-07 23:00:54 +0800

    Part 9/ 1&2 complete
    Some simple inheritance exercises and call to super

--- part09-Part09_01.ABC/src/main/java/Main.java
+++ part09-Part09_01.ABC/src/main/java/Main.java
@@ -3,6 +3,17 @@ public class Main {
 
     public static void main(String[] args) {
         // write your test code here
+        A a = new A();
+        B b = new B();
+        C c = new C();
+
+        a.a();
+        b.b();
+        c.c();
         
+        // Inheritance 
+        c.a();
+        c.b();
+        c.c();
     }
 }
--- part09-Part09_02.PersonAndSubclasses/src/main/java/Main.java
+++ part09-Part09_02.PersonAndSubclasses/src/main/java/Main.java
@@ -1,8 +1,38 @@
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-        // write your test code here
-    }
+        // test person
+        Teacher ada = new Teacher("Ada Lovelace", "24 Maddox St. London W1S 2QN", 1200);
+        Teacher esko = new Teacher("Esko Ukkonen", "Mannerheimintie 15 00100 Helsinki", 5400);
+        System.out.println(ada);
+        System.out.println(esko);
 
+        Student ollie = new Student("Ollie", "6381 Hollywood Blvd. Los Angeles 90028");
+
+        int i = 0;
+        while (i < 25) {
+            ollie.study();
+            i = i + 1;
+        }
+        System.out.println(ollie);
+        
+        System.out.println("");
+        
+        //testing printPersons method 
+        ArrayList<Person> persons = new ArrayList<Person>();
+        persons.add(new Teacher("Ada Lovelace", "24 Maddox St. London W1S 2QN", 1200));
+        persons.add(new Student("Ollie", "6381 Hollywood Blvd. Los Angeles 90028"));
+
+        printPersons(persons);
+    }
+    //  prints all the persons on the list given as the parameter
+    public static void printPersons(ArrayList<Person> persons) {
+
+        for (Person person : persons) {
+            System.out.println(person);
+        }
+    }
 }
