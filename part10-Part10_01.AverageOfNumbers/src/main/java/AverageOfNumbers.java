4b8d2df19639e1e07066353aba31e7b5bcad77d9
RichiCod3s <aaronrichardsonsg@gmail.com>
2024-03-14 21:21:41 +0800

    Part 10/ 1&2 complete
    Streams
    You can use streams on any collection type. I mapToInt to change String/Integer type to int, filter, average which also has getAsDouble.

--- part10-Part10_01.AverageOfNumbers/src/main/java/AverageOfNumbers.java
+++ part10-Part10_01.AverageOfNumbers/src/main/java/AverageOfNumbers.java
@@ -6,7 +6,24 @@ public class AverageOfNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        ArrayList<String> input = new ArrayList<>();
         // Write your program here
-
+        System.out.println("Input numbers, type \"end\" to stop");
+        while(true){
+            String row = scanner.nextLine();
+            if(row.equals("end")){
+                break;
+            }
+            
+            input.add(row);
+        }
+    
+    
+    double average = input.stream().mapToInt(s -> Integer.valueOf(s)).average().getAsDouble();
+    
+        System.out.println("average of the numbers: " + average);
+        
+        // ANOTHER WAY: You can also enter the stream inside the sysout
+        // System.out.println("average of the numbers: " + input.stream().mapToInt(i -> i).average().getAsDouble());
     }
 }
--- part10-Part10_02.AverageOfSelectedNumbers/src/main/java/AverageOfSelectedNumbers.java
+++ part10-Part10_02.AverageOfSelectedNumbers/src/main/java/AverageOfSelectedNumbers.java
@@ -6,7 +6,30 @@ public class AverageOfSelectedNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        // toteuta ohjelmasi tänne
-
+        ArrayList<String> input = new ArrayList<>();
+        
+        System.out.println("Input numbers, type\"end\" to stop");
+        while(true){
+            String row = scanner.nextLine();
+            
+            if(row.equals("end")){
+                break;
+            }
+            
+            input.add(row);
+        }
+        
+        System.out.println("Print the average of the negative numbers or the positive numbers?(n/p)");
+        String choice = scanner.nextLine();
+        if(choice.equals("n")){
+            // using a stream on collection(ArrayList), changes String to int, filters to negative numbers, gets the average and converts to double
+            System.out.println(input.stream().mapToInt(s -> Integer.valueOf(s)).filter(number -> number<0).average().getAsDouble());
+        }else if(choice.equals("p")){
+            // exact same but filters to positive numbers
+            System.out.println(input.stream().mapToInt(s -> Integer.valueOf(s)).filter(number -> number>0).average().getAsDouble());
+        }
+        
+        // note to self if you had an Array<Integer> instead of String, you would still need the mapToInt tp convert from reference to :
+        //System.out.println("Average of the negative numbers: " + input.stream().filter(l -> l < 0).mapToInt(i -> i).average().getAsDouble());
     }
 }
