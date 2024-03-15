9ae82bc0a7249dbd1db0c5aed84eb913e64d8f8b
RichiCod3s <aaronrichardsonsg@gmail.com>
2024-03-15 22:52:47 +0800

    Part 10/3 Streams
    Used stream command collect toList() & Collectors.toCollection(ArrayList::new)
    
    Puts values in the stream in to a list/ArrayList

--- part10-Part10_03.PositiveNumbers/src/main/java/PositiveNumbers.java
+++ part10-Part10_03.PositiveNumbers/src/main/java/PositiveNumbers.java
@@ -1,4 +1,5 @@
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
@@ -6,9 +7,30 @@ import java.util.stream.Collectors;
 public class PositiveNumbers {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
         // test your method here
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(1);
+        numbers.add(-2);
+        numbers.add(3);
+        numbers.add(-4);
+        numbers.add(5);
 
+        System.out.println("Original numbers: " + numbers);
+
+        List<Integer> positiveNumbers = positive(numbers);
+
+        System.out.println("Positive numbers: " + positiveNumbers);
     }
 
-}
+    //receives an ArrayList of integers, and returns the positive integers from the list.
+    public static List<Integer> positive(List<Integer> numbers) {
+
+        //Collectors.toCollection(ArrayList::new) command.
+        ArrayList<Integer> positiveNumbers = numbers.stream().filter(i -> i > 0).collect(Collectors.toCollection(ArrayList::new));
+        return positiveNumbers;
+        
+        //Other way: Collectors.toList() 
+        //return numbers.stream().filter(number -> number > 0).collect(Collectors.toList());
+    }
+
+}// class
