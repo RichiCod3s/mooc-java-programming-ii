a7c235c67383f6559d117e00f9c2235fb09a6ed7
RichiCod3s <aaronrichardsonsg@gmail.com>
2024-04-30 21:44:12 +0800

    Random Numbers

--- part12-Part12_06.Numbers/src/main/java/Program.java
+++ part12-Part12_06.Numbers/src/main/java/Program.java
@@ -6,6 +6,21 @@ public class Program {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        
+        System.out.println("How many random numbers should be printed?");
+        
+        int howMany = scanner.nextInt();
+        
+        for(int i=0; i<howMany; i++){
+            //The printed numbers should be within the range [0, 10]
+            int randomNumber = random.nextInt(11);
+            System.out.println(randomNumber);
+            
+            //  System.out.println(random.nextInt(11));
+        }
+        
+        
     }
 
 }
--- part12-Part12_08.Lottery/src/main/java/LotteryRow.java
+++ part12-Part12_08.Lottery/src/main/java/LotteryRow.java
@@ -15,16 +15,30 @@ public class LotteryRow {
         return this.numbers;
     }
 
+    //The numbers are in range 1—40, and 7 numbers will be drawn in total.
     public void randomizeNumbers() {
         // Initialize the list for numbers
         this.numbers = new ArrayList<>();
         // Implement the random number generation here
         // the method containsNumber is probably useful
+        Random random = new Random();
+     
+        while (numbers.size() < 7) {
+            int number = random.nextInt(40) + 1;
+            if (!containsNumber(number)) {
+                numbers.add(number);
+            }
+
+        }
     }
 
     public boolean containsNumber(int number) {
         // Check here whether the number is among the drawn numbers
+        for (int num : this.numbers) {
+            if (num == number) {
+                return true;
+            }
+        }
         return false;
     }
 }
-
