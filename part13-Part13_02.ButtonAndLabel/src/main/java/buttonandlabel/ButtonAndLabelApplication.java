ab44df553ce77489537999d4b10119f5aafa5c90
RichiCod3s <aaronrichardsonsg@gmail.com>
2024-05-04 22:19:04 +0800

    Java FX - UI Components
    Components - Button, Label, TextFields
    
    Component Groups - Group, FlowPane (we add components to the CG).
    
    Scene - Like a JPanel, we add CG to the Stage - Like a JFrame (Window), we add the scene to the Stage.

--- part13-Part13_02.ButtonAndLabel/src/main/java/buttonandlabel/ButtonAndLabelApplication.java
+++ part13-Part13_02.ButtonAndLabel/src/main/java/buttonandlabel/ButtonAndLabelApplication.java
@@ -1,11 +1,49 @@
 package buttonandlabel;
 
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.FlowPane;
+import javafx.stage.Stage;
 
-public class ButtonAndLabelApplication {
+//remember to extend Application otherwise can't use launch in main (Needs an import)
+public class ButtonAndLabelApplication extends Application {
 
+    public void start(Stage window) {
+
+        // components
+        Button button = new Button("Click me");
+        button.setLayoutX(300); // Set X-coordinate
+        button.setLayoutY(300); // Set Y-coordinate
+
+        Label label = new Label("Stage, scene, scene-graph, nodes");
+        label.setLayoutX(0);
+        label.setLayoutY(0);
+       
+        // component group
+        FlowPane root = new FlowPane();
+        //Group root = new Group(); .// seems to have more flexible layout of components compared to group.
+        
+        // add component to component to component group
+        root.getChildren().add(label);
+        root.getChildren().add(button);
+
+        // pass in componentgroup to scene
+        Scene scene = new Scene(root);
+
+        //Stage
+        window.setTitle("My Window Title");
+        window.setHeight(500);
+        window.setWidth(500);
+        window.setScene(scene); // add scene
+        window.show();
+    }
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(ButtonAndLabelApplication.class);
+
     }
 
 }
--- part13-Part13_03.ButtonAndTextField/src/main/java/buttonandtextfield/ButtonAndTextFieldApplication.java
+++ part13-Part13_03.ButtonAndTextField/src/main/java/buttonandtextfield/ButtonAndTextFieldApplication.java
@@ -1,11 +1,46 @@
 package buttonandtextfield;
 
 
-public class ButtonAndTextFieldApplication {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.FlowPane;
+import javafx.stage.Stage;
+import javafx.scene.paint.Color;
 
+public class ButtonAndTextFieldApplication extends Application {
+
+    public void start(Stage window) {
+
+        // components
+        Button button = new Button("click here");
+        TextField text = new TextField();
+        text.setText("My text field");
+
+        // add components to component group
+        FlowPane componentGroup = new FlowPane();
+        componentGroup.getChildren().add(button);
+        componentGroup.getChildren().add(text);
+        
+        // scene
+        // add components to scene
+        Scene scene = new Scene(componentGroup, Color.RED );
+        
+        
+        
+        //stage
+        window.setScene(scene);
+        window.setTitle("Button and Textfield");
+        window.show();
+        
+        
+
+    }
 
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        launch(ButtonAndTextFieldApplication.class);
     }
 
 }
