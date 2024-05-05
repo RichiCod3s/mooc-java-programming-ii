2e6517c971f0c269dd1221db1c2b81bf87d01fec
RichiCod3s <aaronrichardsonsg@gmail.com>
2024-05-05 21:48:53 +0800

    Java FX - adding component groups to BorderPane

--- part13-Part13_04.BorderPane/src/main/java/borderpane/BorderPaneApplication.java
+++ part13-Part13_04.BorderPane/src/main/java/borderpane/BorderPaneApplication.java
@@ -1,11 +1,43 @@
 package borderpane;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
-public class BorderPaneApplication {
 
+public class BorderPaneApplication extends Application {
+    
+    // start method needed to initalise stage
+    @Override
+    public void start(Stage window){
+        
+          BorderPane layout = new BorderPane();
+          
+          //components
+          Label north = new Label("NORTH");
+          layout.setTop(north); // add label to top of BorderPane
+          //another way
+          layout.setBottom(new Label("SOUTH"));
+          layout.setRight(new Label("EAST"));
+          
+          //scene add component group BorderPane
+          Scene scene = new Scene(layout);
+          
+          // add scene to stage
+          window.setScene(scene);
+          window.show();
+    }
+  
+    
+   
+    
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+     
+        launch(BorderPaneApplication.class);
     }
 
+   
 }
--- part13-Part13_05.TextStatistics/src/main/java/textstatistics/TextStatisticsApplication.java
+++ part13-Part13_05.TextStatistics/src/main/java/textstatistics/TextStatisticsApplication.java
@@ -1,11 +1,48 @@
 package textstatistics;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
 
-public class TextStatisticsApplication {
+public class TextStatisticsApplication extends Application {
 
+//initilise Stage
+    public void start(Stage window) {
+
+        // main component group. can add CG and components to left,right,top,bottom
+        BorderPane layout = new BorderPane();
+
+        //text area
+        TextArea text = new TextArea("Write in here");
+        layout.setCenter(text);
+        //Another way to add TextArea to center of BorderPane 
+        //componentGroup.setCenter(new TextArea()); 
+
+        // horizontal box
+        HBox textComponents = new HBox();
+
+        textComponents.getChildren().add(new Label("Letters: 0"));
+        textComponents.getChildren().add(new Label("Words: 0"));
+        textComponents.getChildren().add(new Label("The longest word is:"));
+        textComponents.setSpacing(20);
+        // add HBox to bottom of BorderPane
+        layout.setBottom(textComponents);
+
+        //add BorderPane to Scene   
+        Scene scene = new Scene(layout);
+
+        //add Scene to Stage
+        window.setScene(scene);
+        window.show();
+
+    }
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(TextStatisticsApplication.class);
     }
 
 }
