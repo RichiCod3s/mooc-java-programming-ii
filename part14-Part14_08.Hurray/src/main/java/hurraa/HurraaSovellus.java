2183d545c997cbee8f41748a2ab0033b6d202706
RichiCod3s <aaronrichardsonsg@gmail.com>
2024-05-24 23:27:55 +0800

    Playing Audio file
    Problem with passing test due to an issue with Maven not being able to locate javafx.scene.media.AudioClip;
    
    I have alter the pom file but still no resolution. The code works. To return and dubug later

--- part14-Part14_08.Hurray/src/main/java/hurraa/HurraaSovellus.java
+++ part14-Part14_08.Hurray/src/main/java/hurraa/HurraaSovellus.java
@@ -4,17 +4,31 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.media.AudioClip;
 import javafx.stage.Stage;
 
+/*
+added the below in the pom file under a similar one with  <artifactId>javafx-controls</artifactId>.
+cant seem to get it to pass because it cant find the java fx media 
+the code works
+   <dependency>
+                    <groupId>org.openjfx</groupId>
+                    <artifactId>javafx-media</artifactId>
+                    <version>11.0.2</version>
+                </dependency>
+ */
 public class HurraaSovellus extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
         BorderPane pane = new BorderPane();
 
-        Button nappi = new Button("Hurraa!");
-        pane.setCenter(nappi);
+        Button button = new Button("Hurraa!");
+        pane.setCenter(button);
 
+        // issue with Maven not being able to find javafx.scene.media.AudioClip;
+        AudioClip audio = new AudioClip("file:Applause-Yannick_Lemieux.wav");
+        button.setOnAction(event -> audio.play());
 
         Scene scene = new Scene(pane, 600, 400);
 
